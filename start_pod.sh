231275034b8c38011f8ed089c8417a81a83008af
Martin Juříček <m.juricek@outlook.com>
2024-12-25 10:40:33 +0100

    Update start_pod.sh

--- start_pod.sh
+++ start_pod.sh
@@ -1,7 +1,9 @@
 ## I.
 # Build docker image
 docker build -t jupyter .
+# tag image to Registry, there can be also used Docker based Registry
 docker tag jupyter localhost:32000/jupyter:latest
+# push image to Microk8s based registry
 docker push localhost:32000/jupyter:latest
 
 ## II.
